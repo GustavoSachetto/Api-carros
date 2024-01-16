1350342cd048d4a6514e92ae932f0344329d945e
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2024-01-16 18:22:19 -0300

    feat(Fuel): adicionando rotas de consulta de combustivel

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,8 @@
 // INCLUI ROTA PADRÃO DA API
 include __DIR__.'/api/v1/default.php';
 
+// INCLUI ROTA DE COMBUSTÍVEIS DA API
+include __DIR__.'/api/v1/fuel.php';
+
 // INCLUI ROTA DE VEÍCULOS DA API
 include __DIR__.'/api/v1/cars.php';
