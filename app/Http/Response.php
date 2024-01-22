0fb2e9e8ea8909905d862ccd8c94d3f906796b2c
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2024-01-22 14:39:33 -0300

    fix(Http): resolvendo erro CORS nas rotas de POST
    Para resolver esse erro tive que adicionar o "Access-Control-Allow-Origin" e "Access-Control-Allow-Headers". Também adicionei as rotas de simulação de solicitação.

--- app/Http/Response.php
+++ app/Http/Response.php
@@ -40,6 +40,7 @@ class Response
         $this->content  = $content;
         $this->addHeader("Content-type", $this->contentType);
         $this->addHeader("Access-Control-Allow-Origin", '*');
+        $this->addHeader("Access-Control-Allow-Headers", '*');
     }
 
     /**
