ae770c71601518583d919255709efccaba96babc
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2024-01-24 13:17:29 -0300

    feat(CarModel): implementando métodos e adicionando rotas de atualização de marcas

--- routes/api/v1/brands.php
+++ routes/api/v1/brands.php
@@ -27,8 +27,26 @@ $obRouter->post('/api/v1/brands', [
     }
 ]);
 
+// ROTA DE CADASTRO DE MARCA (PUT)
+$obRouter->put('/api/v1/brands/{id}', [
+    'middlewares' => [
+        'user-basic-auth',
+        'user-admin-auth'
+    ],
+    function($request, $id) {
+        return new Response(200, Service\Brand::setEditBrand($request, $id));
+    }
+]);
+
 // ROTA DE TESTE MARCA (OPTIONS)
 $obRouter->options('/api/v1/brands', [
+    function($request) {
+        return new Response(200, Service\Brand::getDetails($request));
+    }
+]);
+
+// ROTA DE TESTE MARCA ID (OPTIONS)
+$obRouter->options('/api/v1/brands/{id}', [
     function($request) {
         return new Response(200, Service\Brand::getDetails($request));
     }
